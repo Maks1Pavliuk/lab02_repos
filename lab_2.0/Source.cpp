6bd077b0a249893665ec1d79123cfd285d7e39da
Maks1Pavliuk <masik3201@gmail.com>
2024-09-19 21:20:17 +0300

    Second branch_02 commit: uncomment z1 & comment z2

--- lab_2.0/Source.cpp
+++ lab_2.0/Source.cpp
@@ -14,10 +14,10 @@ int main() {
     cout << "x = ";
     cin >> a;
     
-  double z1 = sin(PI / 2 + 3 * a) / (1 - sin(3 * a - PI));  // результат обчислення 1-го виразу
-    // double z2 = 1 / tan(5 * PI / 4 + 3 * a / 2); // результат обчислення 2-го виразу
-    cout << "z1 = " << z1 << endl;
-    // cout << "z2 = " << z2 << endl;
+  //double z1 = sin(PI / 2 + 3 * a) / (1 - sin(3 * a - PI));  // результат обчислення 1-го виразу
+   double z2 = 1 / tan(5 * PI / 4 + 3 * a / 2); // результат обчислення 2-го виразу
+   // cout << "z1 = " << z1 << endl;
+     cout << "z2 = " << z2 << endl;
 
 
     return 0;
